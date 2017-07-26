cc2003ddb70f7d28336c63e7325c6833ad95581d
deve_maximus <30285010+CoderTM@users.noreply.github.com>
2017-07-26 19:21:11 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -15,80 +15,47 @@ install() {
 		sudo apt-get install libstdc++6 -y
 		sudo apt-get install lua-lgi -y
 		sudo apt-get install libnotify-dev -y
+		sudo service redis-server restart
 		wget https://valtman.name/files/telegram-cli-1222
 		mv telegram-cli-1222 tgcli
 		chmod +x tgcli
 		cd ..
 		chmod +x bot
 		chmod +x tg
-		chmod +x autobd.sh
 }
 
 function print_logo() {
-	green "   _____               _                 _______   __  __ "
+	echo -e "\033[38;5;600m"
+        green "    ____               _                 _______   __  __ "
 	green "  / ____|             | |               |__   __| |  \/  |"
 	green " | |        ___     __| |   ___   _ __     | |    | \  / |"
 	green " | |       / _ \   / _` |  / _ \ | '__|    | |    | |\/| |"
         green " | |____  | (_) | | (_| | |  __/ | |       | |    | |  | |"
         green "  \_____|  \___/   \__,_|  \___| |_|       |_|    |_|  |_|"
-	echo -e "\n\e[0m"
+	echo -e "\n\e[36m"
 }
-
-function logo_play() {
-    declare -A txtlogo
-    seconds="0.010"
-    txtlogo[1]="   _____               _                 _______   __  __ "
-    txtlogo[2]="  / ____|             | |               |__   __| |  \/  |"
-    txtlogo[3]=" | |        ___     __| |   ___   _ __     | |    | \  / |"
-    txtlogo[4]=" | |       / _ \   / _` |  / _ \ | '__|    | |    | |\/| |"
-    txtlogo[5]=" | |____  | (_) | | (_| | |  __/ | |       | |    | |  | |"
-    txtlogo[6]="  \_____|  \___/   \__,_|  \___| |_|       |_|    |_|  |_|"
-    printf "\e[31m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-	echo -e "\e[0m"
-}
-
-function beyondteam() {
-	echo -e "\e[0m"
-	green "     >>>>                       join to @CoderTeam                            "
-	echo -e "\e[0m"
-}
-
-red() {
-  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
-}
-green() {
-  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
-}
-white() {
-  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
-}
-update() {
-	git pull
-}
-
 if [ "$1" = "install" ]; then
-	print_logo
-	coderteam
-	logo_play
-	install
+  install
   else
+
 if [ ! -f ./tg/tgcli ]; then
     echo "tg not found"
     echo "Run $0 install"
     exit 1
  fi
-	print_logo
-	coderteam
-	logo_play
+
+
+   print_logo
+   echo -e "\033[38;5;208m"
+   echo -e "     >>>>        @CoderTeam                                  "
+   echo -e "     >>>>        @CoderTeam                                  "
+   echo -e "     >>>>        @CoderTeam                                  "
+   echo -e "     >>>>        @CoderTeam                                  "
+   echo -e "     >>>>        @CoderTeam                                  "
+   echo -e "\033[0;00m"
+   echo -e "\e[36m"
+   logo_play
    #sudo service redis-server restart
-   ./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
-   #./tg/tgcli -s ./bot/bot.lua $@
+   #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
+   ./tg/tgcli -s ./bot/bot.lua $@
 fi
